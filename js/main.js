3fe67600029f2b44a21eb4c73760c82fb000ff7c
Brandon Cacsire <bc@BCs-MacBook-Air.lehman.edu>
2023-02-15 13:32:12 -0500

    js for making a mark on the bard created

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init() {
+ board = ['','','','','','','','',''];
+ render();
+};
+init();
+
+function render() {
+ board.forEach((mark, index) => {
+  squares[index].textContent = mark;
+ });
+};
