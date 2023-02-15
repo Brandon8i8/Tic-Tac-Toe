da747dd167ac8c549ecf3f2f7d6c7cfc9f23c71b
Brandon Cacsire <bc@BCs-MacBook-Air.lehman.edu>
2023-02-15 13:40:49 -0500

    js for taking turns created

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+
 /*----- functions -----*/
+function handleTurn(event) {
+ let idx = squares.findIndex(square => {
+  return square == event.target;
+ });
+ board[idx] = turn;
+ console.log(board);
+};
+
+
 function init() {
  board = ['','','','','','','','',''];
  render();
