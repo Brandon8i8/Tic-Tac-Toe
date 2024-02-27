245da6491676c9e239ab46dfbd16223daf07923a
Brandon <b.cacsire8@gmail.com>
2024-02-26 22:43:28 -0500

    add ability to take turns

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
+function handleTurn(event) {
+  let idx = squares.findIndex(function(square) {
+    return square == event.target;
+  });
+
+  board[idx] = turn;
+  console.log(board);
+};
+
 function init() {
   board = ['','','','','','','','',''];
 
