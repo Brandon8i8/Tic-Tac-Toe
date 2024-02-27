f27565ab2e7dab2de837456ef8b6c4ebb6a9bb2c
Brandon <b.cacsire8@gmail.com>
2024-02-26 23:05:38 -0500

    added logic for winning

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 /*----- constants -----*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
@@ -11,13 +23,21 @@ const messages = document.querySelector('h2');
 document.getElementById('board').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
+function getWinner() {
+  let winner = null;
+  winningCombos.forEach(function(combo, index) {
+    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+  });
+  return winner;
+};
+
 function handleTurn(event) {
   let idx = squares.findIndex(function(square) {
     return square === event.target;
   });
 
   board[idx] = turn;
-  turn = turn === 'X' ? 'O' : 'X';
+  win = getWinner();
   render();
 };
 
@@ -31,5 +51,5 @@ function render() {
   board.forEach(function(mark, index) {
     squares[index].textContent = mark;
   });
-  messages.textContent = `It's ${turn}'s turn!`;
+  messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
